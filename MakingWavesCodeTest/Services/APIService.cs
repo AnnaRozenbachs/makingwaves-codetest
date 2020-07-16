a10bc412ad37f21d42ca951524442c2f95f94ffc
AnnaRozenbachs <57345254+AnnaRozenbachs@users.noreply.github.com>
2020-07-16 16:36:20 +0200

    projectfiles

---
+++ MakingWavesCodeTest/Services/APIService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace MakingWavesCodeTest.Services
+{
+    public class APIService
+    {
+        public void CreateRequest()
+        {
+            var request = WebRequest.Create("https://reqres.in/api/example?per_page=2&page=1");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            var response = request.GetResponse();
+
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = reader.ReadToEnd();
+            }
+        }
+    }
+}
